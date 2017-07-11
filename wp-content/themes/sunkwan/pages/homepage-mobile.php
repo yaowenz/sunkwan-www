64b9f3045e6b091afe85b420b12b98c2df15f7fb
Yaowen Zhang <yaowen.zhang@live.com>
2017-07-11 21:29:41 +0800

    update

--- wp-content/themes/sunkwan/pages/homepage-mobile.php
+++ wp-content/themes/sunkwan/pages/homepage-mobile.php
@@ -30,16 +30,25 @@ jQuery(function($) {
 			</li>
 		</ul>
 	</div>	
+	<div id="index-about">
+		<div class="about-text">
+			<h2 class="header">关于上坤</h2>
+			<div class="sep line"></div>
+			<div class="text">上坤股份成立于2009年，系深圳盈信集团投资有限公司成员企业，以房地产开发、地产金融、商业资产管理为公司主营业务。</div>
+			<div class="text">上坤精耕上海，城市发展布局长三角核心经济城市和深广经济圈，旨在打造更宜居的产品，拒绝产品复制、关注客户体验，不断从使用者的角度出发，做出“被需要”的产品，用设计、科技、人文赋予产品全新的生命力。</div>
+			<div class="link-button"><a href="http://server-dev/sunkwan/www/about">查看更多</a></div>
+		</div>
+	</div>
 	<div id="index-culture">	
 		<div class="culture-image">
 			<img src="<?php echo get_template_directory_uri()?>/images/mobile/index-culture.jpg" width="100%" style="vertical-align:bottom" />
 		</div>
 		<div class="culture-text">
-			<h2 class="header">关于上坤</h2>
+			<h2 class="header red">品牌文化</h2>
 			<div class="sep line"></div>
-			<div class="text">上坤股份成立于2009年，系深圳盈信集团投资有限公司成员企业，以房地产开发、地产金融、商业资产管理为公司主营业务。</div>
-			<div class="text">上坤精耕上海，城市发展布局长三角核心经济城市和深广经济圈，旨在打造更宜居的产品，拒绝产品复制、关注客户体验，不断从使用者的角度出发，做出“被需要”的产品，用设计、科技、人文赋予产品全新的生命力。</div>
-			<div class="link-button"><a href="<?php echo site_url('about')?>">查看更多</a></div>
+			<div class="text">为宜居而来</div>
+			<div class="text">满足客户对家与美好生活的想象</div>
+			<div class="link-button"><a href="<?php echo site_url('spirit')?>">查看更多</a></div>
 		</div>
 	</div>
 	<div id="index-branches">		
@@ -76,15 +85,13 @@ jQuery(function($) {
 			?>
 		</ul>
 		<div class="link-button"><a href="<?php echo site_url('category/news')?>">查看更多</a></div>
-	</div>
+	</div>	
 	<div id="index-video">	
 		<h2 class="header red">企业视频</h2>
 		<div class="sep line"></div>
 		<div style="width:80%;margin:auto">
 			<video id="sunkwan_video" width="100%" height="auto" preload="metadata" controls="controls" poster="<?php echo get_template_directory_uri()?>/images/mobile/index-video.jpg"><source src="<?php echo site_url('sunkwan-video.mp4')?>" type="video/mp4"></video>
 		</div>
-	</div>	
+	</div>
 </div>
-
-
 <?php get_footer();?>
