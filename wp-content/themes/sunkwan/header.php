194284755eb9b9c3a34360371685b64d74fbf70a
Yaowen Zhang <yaowen.zhang@live.com>
2017-11-03 07:07:27 +0800

    增加统计

--- wp-content/themes/sunkwan/header.php
+++ wp-content/themes/sunkwan/header.php
@@ -10,6 +10,15 @@
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>	
 	<?php endif; ?>
 	<?php wp_head(); ?>
+	<script>
+		var _hmt = _hmt || [];
+		(function() {
+		  var hm = document.createElement("script");
+		  hm.src = "https://hm.baidu.com/hm.js?42bec918e18b6b2f9163c1bb674502ac";
+		  var s = document.getElementsByTagName("script")[0]; 
+		  s.parentNode.insertBefore(hm, s);
+		})();
+	</script>
 </head>
 <body <?php body_class(); ?>>
 <?php
