5c779ba8791a1bc21e42ba252ef25e9ea333c5a4
Yaowen Zhang <yaowen.zhang@live.com>
2018-06-27 01:55:49 +0800

    update 企业文化 bug fix 新闻列表 mobile 增加iPad

--- wp-content/themes/sunkwan/pages/spirit.php
+++ wp-content/themes/sunkwan/pages/spirit.php
@@ -2,8 +2,7 @@
 <div class="primary content spirit">
 	<div class="article">
 		<div class="spirit-title">
-			<img
-				src="<?php echo get_template_directory_uri()?>/images/spirit-title.png" />
+			<img src="<?php echo get_template_directory_uri()?>/images/spirit-title.png" />
 		</div>
 		<div class="spirit-text">
 			<p>
@@ -12,19 +11,60 @@
 			</p>
 		</div>
 		<div class="spirit-bg">
-			<img
-				src="<?php echo get_template_directory_uri()?>/images/spirit-text-bg.jpg"
-				width="100%" />
+			<img src="<?php echo get_template_directory_uri()?>/images/spirit-text-bg.jpg" width="100%" />
 		</div>
 		<div style="float: left; width: 50%" class="core-value">
-			<img
-				src="<?php echo get_template_directory_uri()?>/images/spirit-core-1.png"
-				width="100%" />
+			<div class="core-value-section">
+				<div class="section-text">
+					<div class="circle">
+						<div class="header">客户<br/>第一</div>
+					</div>
+					<div class="text">不仅关注客户的痛点，更关注客户的爽点，把满足客户的期望和需求放在首位。</div>
+				</div>
+			</div>
+			<div class="core-value-section">
+				<div class="section-text">
+					<div class="circle">
+						<div class="header">团队<br/>协作</div>
+					</div>
+					<div class="text">只有完美的团队，才有完美的个人。理性看待自身能力，欣赏团队成员优点。</div>
+				</div>
+			</div>
+			<div class="core-value-section">
+				<div class="section-text">
+					<div class="circle">
+						<div class="header">阳光<br/>激情</div>
+					</div>
+					<div class="text">坚持一切事情都可以放在阳光下沟通。心有阳光，积极向上，坚持正向能量传递、不轻言放弃。</div>
+				</div>
+			</div>
+			<div class="clear"></div>
 		</div>
 		<div style="float: left; width: 50%" class="core-value">
-			<img
-				src="<?php echo get_template_directory_uri()?>/images/spirit-core-2.png"
-				width="100%" />
+			<div class="core-value-section">
+				<div class="section-text">
+					<div class="circle">
+						<div class="header">专业<br/>极致</div>
+					</div>
+					<div class="text">要努力成为行业内的专业与权威，以目标为导向，对作品提升精益求精，对细节把控无可挑剔。</div>
+				</div>
+			</div>
+			<div class="core-value-section">
+				<div class="section-text">
+					<div class="circle">
+						<div class="header">拥抱<br/>变化</div>
+					</div>
+					<div class="text">主动拥抱变化，<br/>在变化中寻机会，<br/>在变化中求成长，<br/>在变化中收获成功。</div>
+				</div>
+			</div>
+			<div class="core-value-section">
+				<div class="section-text">
+					<div class="circle">
+						<div class="header">尊重<br/>分享</div>
+					</div>
+					<div class="text">尊重每一位员工和每一个合作伙伴，上坤倡导与团队同心共享的分享文化。</div>
+				</div>
+			</div>
 		</div>
 		<div class="clearfix"></div>
 		<div class="section spirit hidden" id="section-spirit-scroll">
@@ -39,9 +79,6 @@
 				<p>为宜居而来，满足客户对家与美好生活的想象。</p>
 				<p class="subtitle">企业理念</p>
 				<p>上坤坚持以专业能力从市场获取公平回报，上坤存在的价值是“满足客户对家与美好生活的想象”；团队成员不断学习、探索如何设计和建造符合时代与客户需求的作品，做到不断更新迭代；肩负时代的责任，为社会提供绿色、环保、节能的作品。
-				
-				
-				
 				<p class="subtitle">核心价值观</p>
 				<ul>
 					<li>客户第一： 不仅关注客户的痛点，更关注客户的爽点，把满足客户的期望和需求放在首位。</li>
